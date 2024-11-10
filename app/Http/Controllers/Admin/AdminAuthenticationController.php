26d806280beecf10f3dd685bba6a5a3eb9764aa1
Renaldy <renaldytherrydota@gmail.com>
2024-11-11 02:36:31 +0800

    complete admin login and logout features

--- app/Http/Controllers/Admin/AdminAuthenticationController.php
+++ app/Http/Controllers/Admin/AdminAuthenticationController.php
@@ -3,9 +3,16 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\AdminResetPasswordRequest;
 use App\Http\Requests\HandleLoginRequest;
+use App\Mail\AdminSendResetLinkMail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\SendResetLinkRequest;
+use App\Models\Admin;
+use Illuminate\Support\Facades\Mail;
+
+
 
 class AdminAuthenticationController extends Controller
 {
@@ -21,7 +28,7 @@ class AdminAuthenticationController extends Controller
        return redirect()->route('admin.dashboard');
     }
 
-    public function logout(Request $request) 
+    public function logout(Request $request)
     {
         Auth::guard('admin')->logout();
 
@@ -30,5 +37,43 @@ class AdminAuthenticationController extends Controller
         $request->session()->regenerateToken();
 
         return redirect()->route('admin.login');
+    }
+
+    public function forgotPassword()
+    {
+        return view('admin.auth.forgot-password');
+    }
+
+    public function sendResetLink(SendResetLinkRequest $request)
+    {
+        $token = \Str::random(64);
+
+        $admin = Admin::where('email', $request->email)->first();
+        $admin->remember_token = $token;
+        $admin->save();
+
+        Mail::to($request->email)->send(new AdminSendResetLinkMail($token, $request->email));
+
+        return redirect()->back()->with('succes', 'A mail has been sent to your email address');
+    }
+
+    public function resetPassword($token)
+    {
+         return view('admin.auth.reset-password', compact('token'));
+    }
+
+    public function handleResetPassword(AdminResetPasswordRequest $request)
+    {
+        $admin = Admin::where(['email' => $request->email, 'remember_token' => $request->token])->first();
+
+        if(!$admin){
+            return back()->with('error', 'token is invalid');
+        }
+
+        $admin->password = bcrypt($request->password);
+        $admin->remember_token = null;
+        $admin->save();
+
+        return redirect()->route('admin.login')->with('succes', 'Password reset succesfull');
     }
 }
--- routes/admin.php
+++ routes/admin.php
@@ -5,9 +5,16 @@ use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'admin', 'as' => 'admin.'], function(){
-    Route::get('login', [AdminAuthenticationController::class, 'login'])->name('login'); 
-    Route::post('login', [AdminAuthenticationController::class, 'handleLogin'])->name('handle-login'); 
-    Route::post('logout', [AdminAuthenticationController::class, 'logout'])->name('logout'); 
+    Route::get('login', [AdminAuthenticationController::class, 'login'])->name('login');
+    Route::post('login', [AdminAuthenticationController::class, 'handleLogin'])->name('handle-login');
+    Route::post('logout', [AdminAuthenticationController::class, 'logout'])->name('logout');
+
+    /**Reset Password */
+    Route::get('forgot-password', [AdminAuthenticationController::class, 'forgotPassword'])->name('forgot-password');
+    Route::post('forgot-password', [AdminAuthenticationController::class, 'sendResetLink'])->name('forgot-password.send');
+
+    Route::get('reset-password/{token}', [AdminAuthenticationController::class, 'resetPassword'])->name('reset-password');
+    Route::post('reset-password', [AdminAuthenticationController::class, 'handleResetPassword'])->name('reset-password.send');
 });
 
 Route::group(['prefix' => 'admin', 'as' => 'admin.' , 'middleware' => ['admin']], function(){
